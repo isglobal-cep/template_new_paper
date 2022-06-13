8ac918902256b2641110e48a5df5c7509da0c64e
Lorenzo Fabbri <lorenzo.fabbri@isglobal.org>
2022-06-13 17:21:17 +0200

    Create rclone_gdrive.sh

---
+++ housekeeping/rclone_gdrive.sh
@@ -0,0 +1,29 @@
+## Script to keep in sync local results with Google Drive
+
+module load utils/rclone
+
+# Store variables and paths of interest
+path_to_dags_local="../DAGs/"
+path_to_dags_remote="gdrive:path"
+
+path_to_paper_local="../output/paper/manuscript.pdf"
+path_to_paper_remote="gdrive:path"
+
+path_to_figures_local="../results/figures/"
+path_to_figures_remote="gdrive:path"
+
+# TODO: posters, presentations #
+
+path_to_tables_local="../results/tables/"
+path_to_tables_remote="gdrive:path"
+
+# Specify which directories/files should be kept in sync with Google Drive
+## DAGs
+rclone sync $path_to_dags_local $path_to_dags_remote
+
+## PDF of paper, posters, presentations
+rclone copy $path_to_paper_local $path_to_paper_remote
+
+## Figures, tables
+rclone sync $path_to_figures_local $path_to_figures_remote
+rclone sync $path_to_tables_local $path_to_tables_remote
