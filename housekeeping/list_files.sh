e601b06bed4be5fb009e2dd2457d0e50c394895b
Lorenzo Fabbri <lorenzo.fabbri@isglobal.org>
2022-06-15 12:35:45 +0200

    Update list_files.sh

--- housekeeping/list_files.sh
+++ housekeeping/list_files.sh
@@ -1,16 +1,16 @@
 ## Update README file of given directory with list of files
 
 # Date and time
-dt = $(date '+%d/%m/%Y %H:%M:%S');
+dt=$(date '+%d/%m/%Y %H:%M:%S');
 
 # Loop over files in given directory
 for entry in "$1"/*
 do
   if [[ -f $entry ]]
   then
-    echo "${entry:2} $dt" >> README.md;
+    echo "${entry:0} $dt" >> "$1/README.md";
   fi
 done
 
 # Sort README file
-sort -o README.md{,}
+sort -o "$1/README.md"{,}
